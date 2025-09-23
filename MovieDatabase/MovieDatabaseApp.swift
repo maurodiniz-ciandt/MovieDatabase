7f53590347d2eea2ab6a0e2c5e3304993c1f9065
maurodiniz-ciandt <maurodiniz@ciandt.com>
2025-09-23 15:57:59 -0300

    Initial Commit

---
+++ MovieDatabase/MovieDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDatabaseApp.swift
+//  MovieDatabase
+//
+//  Created by Mauro Augusto Araujo Diniz on 23/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieDatabaseTests/MovieDatabaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDatabaseTests.swift
+//  MovieDatabaseTests
+//
+//  Created by Mauro Augusto Araujo Diniz on 23/09/25.
+//
+
+import Testing
+@testable import MovieDatabase
+
+struct MovieDatabaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
